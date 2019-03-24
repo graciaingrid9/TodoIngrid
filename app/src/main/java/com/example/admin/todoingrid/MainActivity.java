c982e3f054968d82ad3339b9255bbe8507ffd2ec
graciaingrid9 <46657828+graciaingrid9@users.noreply.github.com>
2019-03-23 20:24:24 -0400

    simple todoIngrid
    Signed-off-by: graciaingrid9 <46657828+graciaingrid9@users.noreply.github.com>

--- app/src/main/java/com/example/admin/todoingrid/MainActivity.java
+++ app/src/main/java/com/example/admin/todoingrid/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.admin.todoingrid;
 
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +14,22 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final Object FileUtils =;
-    ArrayList<String> items = new ArrayList<String>(FileUtils.readLines(getDataFile(),Charset.defaultCharset()));
+    // a numeric code to identify the edit activity
+    public final static int EDIT_REQUEST_CODE = 20;
+    //keys used for passing data between activities
+    public final static String  ITEM_TEXT = "itemText";
+    public final static String ITEM_POSITION = "itemPosition";
+
+
+    ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
 
@@ -51,16 +62,53 @@ private void setupListViewListtener() {
     lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
         @Override
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-            Log.i("MainActivity", ("Item remove from list");
+            Log.i("MainActivity", "Item remove from list:" + position);
             items.remove(position);
             itemsAdapter.notifyDataSetChanged();
             writeItems();
             return true;
         }
     });
+
+    // set up item listener for edit (regular click)
+    lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            //create the new activity
+            Intent i = new Intent (MainActivity.this, EditItemActivity.class);
+            //pass the data being edited
+            i.putExtra(ITEM_TEXT, items.get(position));
+            i.putExtra(ITEM_POSITION, position);
+            //display the activity
+            startActivityForResult(i, EDIT_REQUEST_CODE);
+        }
+    });
 }
 
-private File getDataFile(){
+//handle results from edit activity
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if the edit activity completed ok
+        if (resultCode==RESULT_OK && requestCode== EDIT_REQUEST_CODE){
+            //extract updated item text from result intent extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            //extract original position of edited item
+            int position = data.getExtras().getInt(ITEM_POSITION);
+            //updaten the model whith the new item text at the edited position
+            items.set(position, updatedItem);
+            //notify the adapter that the model changed
+            itemsAdapter.notifyDataSetChanged();
+            //persist the changed model
+            writeItems();
+            //notify the user the operation completed ok
+            Toast.makeText(this,"Item updated successfully",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File getDataFile(){
         return new File(getFilesDir(),"todo.txt");
     }
 
